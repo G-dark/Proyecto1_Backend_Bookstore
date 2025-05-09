b8d17b37248f777e0b48f80196a18c5b7aca8f27
Edi <edisonpacheco56@hotmail.com>
2025-05-09 11:57:31 -0500

    fix:RecoverydeletedUserANDenv.exampleCompleted

--- src/User/create.user.action.ts
+++ src/User/create.user.action.ts
@@ -1,19 +1,36 @@
-import  User  from "./user.model.js";
-import {userType} from "./user.model.js";
+import User from "./user.model.js";
+import { userType } from "./user.model.js";
 import bcrypt from "bcryptjs";
 import { SALT_OR_ROUNDS, PEPPER } from "../App/config.js";
 
-const createUser =async (user:userType):Promise<any>=>{
-    try{
-        const userWithPepper = user.password + PEPPER;
-        user.password = await bcrypt.hash(userWithPepper, SALT_OR_ROUNDS);
-        const newUser = new User(user);
-        await newUser.save();
-        return {success: "Usuario registrado"};
-    }catch (error) {
-        console.error("Hubo un error",error);
-        return {error: "Error al registrar usuario"};
+const createUser = async (user: userType): Promise<any> => {
+  try {
+    const userWithPepper = user.password + PEPPER;
+    user.password = await bcrypt.hash(userWithPepper, SALT_OR_ROUNDS);
+    const result = await User.find({ email: user.email });
+    if (result.length == 0) {
+      const newUser = new User(user);
+      const created = await newUser.save();
+      if (created) {
+        return { success: "Usuario registrado" };
+      } else {
+        return { error: "Usuario no registrado" };
+      }
+    } else {
+      const result2 = await User.updateOne(
+        { email: user.email },
+        { deleted: false, deleteAt: null }
+      );
+      if (result2.modifiedCount != 0) {
+        return { success: "Usuario registrado" };
+      } else {
+        return { error: "Usuario no registrado" };
+      }
     }
-}
+  } catch (error) {
+    console.error("Hubo un error", error);
+    return { error: "Error al registrar usuario" };
+  }
+};
 
 export default createUser;
